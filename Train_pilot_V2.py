b2607d16b12468e9e389a3ad0cf0ab4cbc99f73e
AIDRI <60047183+AIDRI@users.noreply.github.com>
2020-05-11 07:58:38 +0200

    Add files via upload

---
+++ Train_pilot_V2.py
@@ -0,0 +1,81 @@
+import numpy as np
+from keras.layers import Dense, Activation, Flatten, Conv2D, Lambda
+from keras.layers import MaxPooling2D, Dropout
+from keras.utils import print_summary
+from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint
+import keras.backend as K
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+
+def keras_model(image_x, image_y):
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 127.5 - 1., input_shape=(image_x, image_y, 1)))
+
+    model.add(Conv2D(12, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2), padding='valid'))
+
+    model.add(Conv2D(24, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2), padding='valid'))
+
+    model.add(Conv2D(36, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2), padding='valid'))
+
+    model.add(Conv2D(72, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2), padding='valid'))
+
+    model.add(Conv2D(144, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2, 2), padding='valid'))
+
+    model.add(Flatten())
+    model.add(Dropout(0.4))
+
+    model.add(Dense(128))
+
+    model.add(Dense(64))
+    model.add(Dense(1))
+
+    model.compile(optimizer='adam', loss="mse")
+    filepath = "Autopilot_10.h5"
+    checkpoint = ModelCheckpoint(filepath, verbose=1, save_best_only=True)
+    callbacks_list = [checkpoint]
+
+    return model, callbacks_list
+
+
+def loadFromPickle():
+    with open("features", "rb") as f:
+        features = np.array(pickle.load(f))
+    with open("labels", "rb") as f:
+        labels = np.array(pickle.load(f))
+
+    return features, labels
+
+
+def main():
+    features, labels = loadFromPickle()
+    features, labels = shuffle(features, labels)
+    train_x, test_x, train_y, test_y = train_test_split(features, labels, random_state=0,
+                                                        test_size=0.3)
+    train_x = train_x.reshape(train_x.shape[0], 100, 100, 1)
+    test_x = test_x.reshape(test_x.shape[0], 100, 100, 1)
+    model, callbacks_list = keras_model(100, 100)
+
+    print_summary(model)
+
+    model.fit(train_x, train_y, validation_data=(test_x, test_y), epochs=20, batch_size=64,
+              callbacks=callbacks_list)
+
+    model.save('Autopilot_10.h5')
+
+
+main()
+K.clear_session();
